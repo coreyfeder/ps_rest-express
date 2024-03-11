94d5f16a275cb3933e289aad60692839f4afcf48
Corey Feder <corey.feder@gmail.com>
2024-03-11 15:33:19 -0400

    post still disembodied

---
+++ data/users.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+      userid: 1,
+      name: "Carey",
+      username: "cyare23",
+      email: "cy23@example.com",
+    },
+    {
+      userid: 2,
+      name: "Mikoto",
+      username: "Miiko",
+      email: "mikoto_u@example.com",
+    },
+    {
+      userid: 3,
+      name: "Ronald",
+      username: "RonRonRon",
+      email: "mronald@example.com",
+    },
+  ];
+  
+  module.exports = users
+  
--- index.js
+++ index.js
@@ -1,63 +1,167 @@
 const express = require("express");
-const fs = require("fs");
-const logging = require("logging");
+const bodyParser = require("body-parser");
 
+const router = express.Router();
 const app = express();
 const port = 3000; // try 5000 if troubles
 const host = "localhost";
 const protocol = "http";
-const url = `${protocol}://${host}:${port}/`
+const root = "/api";
+const url = `${protocol}://${host}:${port}${root}`;
+
+const users = require("./data/users");
+const posts = require("./data/posts");
 
 // CONNECTION TO DB
 
 // MIDDLEWARE
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json({extended: true}))
+
+// `pass()` => pass control to the next middleware function
+// `pass("router")` => pass control back out of the router instance => skip the rest of the router’s middleware functions
+
+// a middleware function with no mount path => code executed for every request to the router
+router.use((req, res, next) => {
+    // console.log(`${Date.now()} : ${req.statusCode} : ${req.method} ${req.url} : ${req.originalUrl}`)
+    console.log([
+            Date.now(), 
+            req.method, 
+            req.originalUrl,
+        ].join(' : '))
+    next()
+    console.log([
+            Date.now(), 
+            req.method,  // why doesn't res.method have this?
+            res.statusCode,  // this doesn't work for some errors?
+            res.statusMessage,  // blanks?
+            // JSON.stringify(res.json()),  // goddamn "[object Object]". How does .json() not know it's JSON?
+        ].join(' : '))
+    // console.log(res.json());
+})
+
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use('/user/:id', (req, res, next) => {
+    console.log('Request URL:', req.originalUrl)
+    next()
+}, (req, res, next) => {
+    console.log('Request Type:', req.method)
+    next()
+})
+
+// mount the router on the app
+app.use('/', router)
+
+
 // ROUTES
 
+//  - ulost?
 app.get("/", (req, res) => {
-    res.send("Hello Express!");
+    res.send("Hello from ps_rest-express!");
 });
 
-app.get("/express", (req, res) => {
-    res.send("Creating routes with Express is simple!");
+//  - 404 to other users
+app.get(`/users`, (req, res) => {
+    res.status(404);
+    res.send(`Don't talk to me like I'm some kind of API. Go tell it to ${root}/users.`);
+    res.json({ error: `APIs are found in the ${root} folder.`})
 });
 
-app.get("/user", (req, res) => {
-    res.send("Received a GET request for user!");
+// users
+app.get(`${root}/users`, (req, res) => {
+    res.json(users)
 });
 
-app.get("/user/:userid", (req, res) => {
-    res.send(`Ah, want to know about user ${req.params.userid}, eh?`);
+// user
+app.get(`${root}/user/:userid`, (req, res, next) => {
+    try {
+        const user = users.find((u) => u.userid == req.params.userid);
+        if (user) res.json(user);
+        next()
+    } catch (error) {
+        console.error(error)
+    }
 });
 
-app.get("/user/:userid/profile", (req, res) => {
+// POST -> user = make a new user
+app.post(`${root}/user`, (req, res) => {
+    // if (toString(req.body) != '{}') {console.log("HEY! HEY!! LOOK AT THIS!")};
+    // console.log(`log req.body: ${req.body}`)  // [object Object]
+    // console.log(req.body)  // {}
+    // console.log(req.body[0])  // undefined
+    // console.log(req.body=={})  // false
+    // console.log(req.body=='{}')  // false
+    // console.dir(`dir req.body: ${req.body}`)  // [object Object]
+    // console.dir(req.body)  // {}
+    // console.log(Array.isArray(req.body))  // falser
+    // console.log(Object.keys(req.body))  // (0) []
+    // console.log(Object.values(req.body))  // (0) []
+    // 
+    // console.dir(Object.keys(req))
+    // (29) ['_events', '_readableState', '_maxListeners', 'socket', 'httpVersionMajor', 'httpVersionMinor', 'httpVersion', 'complete', 'rawHeaders', 'rawTrailers', 'joinDuplicateHeaders', 'aborted', 'upgrade', 'url', 'method', 'statusCode', 'statusMessage', 'client', '_consuming', '_dumped', 'next', 'baseUrl', 'originalUrl', '_parsedUrl', 'params', 'query', 'res', 'body', 'route']
+    // console.dir(req.headers)
+    // {user-agent: 'PostmanRuntime/7.36.3', accept: '*/*', postman-token: '9238024e-1a9f-45e1-866a-1b6875eb7856', host: 'localhost:3000', accept-encoding: 'gzip, deflate, br', …}
+    // console.dir(req.readable)  // true
+    // console.dir(req.params)  // blank
+    // console.dir(req.body)  // blank
+    if (!req.body.name || !req.body.username || !req.body.email) {
+        res.status(400)
+        res.json({error: "Insufficient data."})
+        return
+    } else if (users.find((u) => u.username == req.body.username )) {
+        res.status(428)
+        res.json({error: "Username is in use."})
+        return
+    } else {
+        const newUser = {
+            // userid: users[users.length - 1].userid + 1,
+            userid: Math.max(users.forEach((u) => u.userid)) + 1,
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+        }
+        users.push(newUser);
+        res.json(users[users.length - 1])
+    }
+});
+
+app.patch(`${root}/user`, (req, res) => { res.send("Received a PATCH request for user! If only it were that easy, eh?"); });
+
+app.delete(`${root}/user`, (req, res) => { res.send("HAHA! It actually IS that easy! But don't do that. People, don't do murders."); });
+
+// user, weird shit
+app.get(`${root}/user/:userid/profile`, (req, res) => {
     res.send(`${req.params.userid} is great. We laugh. We cry. Better than <i>Cats</i>.`);
-});
+});    
+app.get(`${root}/user/:userid/profile/:data`, (req, res) => {
+    res.send(`Oh no, that'd be an invasion of privacy. ${req.params.userid} probably doesn't want ${req.params.data} out in the open.`);
+});    
 
-app.get("/user/:userid/profile/:data", (req, res) => {
-    res.send(`Oh no, that'd be an invasion of provacy. ${req.params.userid} probably doesn't want ${req.params.data} out in the open.`);
-});
+// posts
+app.get(`${root}/posts`, (req,res) => { res.json(posts) })
 
-app.get("/user/:userid/profile/:data", (req, res) => {
-    res.send(
-      `Navigated to the user profile page for: ${req.params.userid}, with the data: ${req.params.data}.`
-    );
-});
+//post
+app.get(`${root}/post/:postid`, (req,res) => {
+    const post = posts.find((p) => p.postid == req.params.postid);
+    if(post) res.json(post);
+    else next();
+})
 
-app.post("/user", (req, res) => {
-    res.send("Received a POST request for user!");
-});
+// ERROR HANDLING ("endware"?)
 
-app.patch("/user", (req, res) => {
-    res.send("Received a PATCH request for user! If only it were that easy, eh?");
+app.use((err, req, res, next) => {
+    // handle 404's
+    console.error(err.stack);
+    res.status(404)
+    res.json({ error: `Resource not found.` });
 });
+// the above is actually a catch-all.
+// You could also do, say, an app.get()
 
-app.delete("/user", (req, res) => {
-    res.send("HAHA! It actually IS that easy! But don't do that. People, don't do murders.");
-});
 
 // LISTENERS
 
 app.listen(port, () => {
-    console.log(`Server listening at:\n  ${url}`);
+    console.log(`Server listening at:  ${url}`);
 });
